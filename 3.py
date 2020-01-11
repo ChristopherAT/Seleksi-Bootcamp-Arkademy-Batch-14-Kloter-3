d2ef7556493ebba548afc9e73de760c9d0fdccc1
Christopher <christopher.at99@gmail.com>
2020-01-11 19:46:16 +0700

    Modifikasi input menggunakan regex dan syntax *argv dalam fungsi (edit: tambah komentar)

--- 3.py
+++ 3.py
@@ -2,8 +2,10 @@ import re
 def printer(teks,*argv):
     # Ambil {a} {b} {c} dari teks dengan regex:
     list_posisi = re.findall(r'\{([0-9]+)\}',teks)
+    # Potong teks sampai sebelum simbol {
     teks = re.match(r'^(.*?)\{',teks).group(1)
     for pos in list_posisi:
+        # tambahkan kata-kata sesuai posisi {a} {b} {c}
         teks+=argv[int(pos)] + ' '
     return teks
 
